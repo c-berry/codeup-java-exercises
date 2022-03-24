7b9cbd914489d9a5f57f59fed2638b501a32e39c
Christopher Berry <christopher.m.berry01@gmail.com>
2022-03-24 17:03:22 -0500

    complete intro to java exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,91 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        //1.
+//        int myFavoriteNumber = 8;
+//        System.out.println(myFavoriteNumber);
+
+        //2.
+//        String myString = "string";
+//        System.out.println(myString);
+
+        //3.
+//        char myString = 'S';
+//        System.out.println(myString);
+
+//      //4.
+//         float myString = 3.14159F;
+
+        //5.
+//        long myNumber = ;
+//        System.out.println(myNumber);
+
+//        //6.
+//        float myNumber = 3.14F;
+//        System.out.println(myNumber);
+
+//        //7.
+//        long myNumber = 123L;
+////        System.out.println(myNumber);
+//
+//        //8.
+//        myNumber = 123;
+//        System.out.println(myNumber);
+//
+//        //9. ???
+//        float myNumber = 3.14F;
+//
+//        //10.
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+        //11.
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+        //12.
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//
+//         String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        Object three =  o;
+//        int three = (int) "three";
+//        System.out.println(three);
+//        System.out.println(o);
+
+        //13.
+//        int x = 4;
+//        x = x + 5;
+        //or
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+        //or
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
+        //or
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+//        System.out.println(y);
+
+
     }
 }
