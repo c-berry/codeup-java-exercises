5e61af2ccad147874d045c4079e50c389ef8daaf
Christopher Berry <christopher.m.berry01@gmail.com>
2022-03-28 16:10:27 -0500

    create class for exercise and complete do-while loops

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,40 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+
+        System.out.println("test");
+
+        //Loop Basics:
+//        int i = 5;
+//        while (i <= 15){
+//            System.out.println(i);
+//            i++;
+//        }
+
+//        int u = 0;
+//        do {
+//            System.out.println(u);
+//            u += 2;
+//        }
+//        while (u <= 100);
+
+//        int u = 100;
+//        do {
+//            System.out.println(u);
+//            u -= 5;
+//        }
+//        while (u > 0);
+
+//        long y = 2;
+//        do {
+//            System.out.println(y);
+//            y *= y;
+//        }
+//        while (y < 1_000_000);
+
+
+
+
+
+
+    }
+}
