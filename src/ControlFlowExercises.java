03f2b37b1372996685455e0e165aa218b280378e
Christopher Berry <christopher.m.berry01@gmail.com>
2022-03-29 11:45:49 -0500

    complete control statements and loops exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("test");
 
@@ -17,24 +21,79 @@ public class ControlFlowExercises {
 //        }
 //        while (u <= 100);
 
-//        int u = 100;
-//        do {
-//            System.out.println(u);
-//            u -= 5;
-//        }
-//        while (u > 0);
-
-//        long y = 2;
+//        int y = 100;
 //        do {
 //            System.out.println(y);
-//            y *= y;
+//            y -= 5;
 //        }
-//        while (y < 1_000_000);
+//        while (y > 0);
+
+//        long t = 2;
+//        do {
+//            System.out.println(t);
+//            t *= t;
+//        }
+//        while (t < 1_000_000);
+
+//        for(int i = 5; i <=15; i++){
+//            System.out.println(i);
+//        }
+//
+//        for(int u = 0; u <= 100; u += 2){
+//            System.out.println(u);
+//        }
+
+//        for(int y = 100; y > 0; y -= 5){
+//            System.out.println(y);
+//        }
+
+//        for(long t = 2; t < 1_000_000; t *= t){
+//            System.out.println(t);
+//        }
+
+        //Fizzbuzz:
+//        for(int r = 1; r <= 100; r++){
+//            if (r % 3 == 0 && r % 5 == 0){
+//                System.out.println("FizzBuzz");
+//            } else if (r % 3 == 0) {
+//                System.out.println("Fizz");
+//            } else if (r % 5 == 0) {
+//                System.out.println("Buzz");
+//            } else {
+//                System.out.println(r);
+//            }
+//        }
+
+        //Table of Powers:
+//        System.out.println("Enter number to increment to:");
+//        int num = sc.nextInt();
+//        System.out.println("Here is your table!");
+//        System.out.println();
+//        System.out.println("number \t|\t squared \t|\t cubed");
+//        System.out.println("------ \t|\t -------- \t|\t -------");
+//        for(int i = 1; i <= num; i++){
+//            System.out.println(i +" \t\t|\t\t"+ i*i +" \t\t|\t\t"+ i*i*i);
+//        }
+
+        //Convert to Letter Grades:
+        System.out.println("Enter your numerical grade:");
+        int grade = sc.nextInt();
+
+        if (grade > 98){
+            System.out.println("A+");
+        } else if (grade > 87) {
+            System.out.println("Grade: A");
+        } else if (grade > 79) {
+            System.out.println("Grade B");
+        } else if(grade > 66){
+            System.out.println("Grade C");
+        } else if (grade > 59) {
+            System.out.println("Grade: D");
+        } else {
+            System.out.println("Grade: F");
+        }
 
 
-
-
-
-
+        //END=>
     }
 }
