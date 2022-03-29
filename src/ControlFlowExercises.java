3c49231e6ba05647c83ad09c13e139cdf8a116dd
Christopher Berry <christopher.m.berry01@gmail.com>
2022-03-29 15:41:35 -0500

    complete first string exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -10,7 +10,7 @@ public class ControlFlowExercises {
         //Loop Basics:
 //        int i = 5;
 //        while (i <= 15){
-//            System.out.println(i);
+//            System.out.print(i + " ");
 //            i++;
 //        }
 
@@ -26,7 +26,7 @@ public class ControlFlowExercises {
 //            System.out.println(y);
 //            y -= 5;
 //        }
-//        while (y > 0);
+//        while (y >= -10);
 
 //        long t = 2;
 //        do {
@@ -55,10 +55,10 @@ public class ControlFlowExercises {
 //        for(int r = 1; r <= 100; r++){
 //            if (r % 3 == 0 && r % 5 == 0){
 //                System.out.println("FizzBuzz");
-//            } else if (r % 3 == 0) {
-//                System.out.println("Fizz");
 //            } else if (r % 5 == 0) {
 //                System.out.println("Buzz");
+//            } else if (r % 3 == 0) {
+//                System.out.println("Fizz");
 //            } else {
 //                System.out.println(r);
 //            }
@@ -75,23 +75,44 @@ public class ControlFlowExercises {
 //            System.out.println(i +" \t\t|\t\t"+ i*i +" \t\t|\t\t"+ i*i*i);
 //        }
 
-        //Convert to Letter Grades:
-        System.out.println("Enter your numerical grade:");
-        int grade = sc.nextInt();
-
-        if (grade > 98){
-            System.out.println("A+");
-        } else if (grade > 87) {
-            System.out.println("Grade: A");
-        } else if (grade > 79) {
-            System.out.println("Grade B");
-        } else if(grade > 66){
-            System.out.println("Grade C");
-        } else if (grade > 59) {
-            System.out.println("Grade: D");
-        } else {
-            System.out.println("Grade: F");
+        System.out.println("Do you want to play again? [y/N]");
+        String answer = sc.next();
+        if (answer.equals("y")) {
+            do {
+                if (answer.equals("y")) {
+                    System.out.println("Enter another number to increment to:");
+                    int num2 = sc.nextInt();
+                    for (int i = 1; i <= num2; i++)
+                        System.out.println(i + " \t\t|\t\t" + i * i + " \t\t|\t\t" + i * i * i);
+                    System.out.println("Continue? [y/N]");
+                    answer = sc.next();
+                    if (!answer.equals("y")) {
+                        System.out.println("Thanks for playing!");
+                    }
+                } else {
+                    System.out.println("Thanks for playing!");
+                    break;
+                }
+            } while (answer.equals("y"));
         }
+
+        //Convert to Letter Grades:
+//        System.out.println("Enter your numerical grade:");
+//        int grade = sc.nextInt();
+//
+//        if (grade > 98){
+//            System.out.println("Grade: A+");
+//        } else if (grade > 87){
+//            System.out.println("Grade: A");
+//        } else if (grade > 79){
+//            System.out.println("Grade: B");
+//        } else if (grade > 66){
+//            System.out.println("Grade: C");
+//        } else if (grade > 59){
+//            System.out.println("Grade: D");
+//        } else {
+//            System.out.println("Grade: F");
+//        }
 
 
         //END=>
---
+++ src/StringExercise.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class StringExercise {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String pinkFloydLyrics = "We don't need no education \nWe don't need no thought control";
+        System.out.println(pinkFloydLyrics);
+
+        String string2 = "Check \"this\" out!, \"s inside of \"s!\"";
+        System.out.println(string2);
+
+        String string3 = "In windows, the main drive is usually C:\\";
+        System.out.println(string3);
+
+        String string4 = "I can do backslashes \\, double backslashes \\\\,\n and the amazing triple backslash \\\\\\!";
+        System.out.println(string4);
+
+    }
+}
