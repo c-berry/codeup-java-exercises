524a10739953c77b0221debe69cba0e4ef7db34f
Christopher Berry <christopher.m.berry01@gmail.com>
2022-04-04 14:06:05 -0500

    complete ServerNameGenerator exercise

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,3 +1,22 @@
+import java.util.Random;
+
 public class ServerNameGenerator {
+
+    private static String[] adjectives = {"happy", "sad", "cool", "hot", "lukewarm", "solid", "translucent", "big", "small", "medium"};
+    private static String[] nouns = {"dog", "cat", "bird", "lizard", "fish", "pig", "lion", "tree", "rock", "cloud"};
+
+
+//    public static String getRandom(String[] array) {
+//        int random = new Random().nextInt(array.length);
+//        return array[random];
+//    }
+
+    public static String getRandom(String[] array) {
+        return array[(int) Math.ceil(Math.random() * array.length - 1)];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getRandom(nouns) + "-" + getRandom(adjectives));
+    }
 
 }
